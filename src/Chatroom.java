ac8e6de2a67bf1a14c04d66bc4e46fe904206569
Vincent <vincentpalomo@yahoo.com>
2023-06-15 19:22:36 -0500

    currently cannot send messages, gui shows up but nothing can be submitted

--- src/Chatroom.java
+++ src/Chatroom.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -32,7 +34,7 @@ public class Chatroom {
         panel.add(messageField);
 
         // send button
-        JButton sendButton = new JButton();
+        JButton sendButton = new JButton("Send");
         panel.add(sendButton);
 
         // frame layout
@@ -42,11 +44,50 @@ public class Chatroom {
         // set font of the chat area
         chatArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
 
+        // padding for the chat area
+        chatArea.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+        // margins for message field
+        messageField.setMargin(new Insets(10,10,10,10));
+
+        // need action listeners for button and message
+        sendButton.addActionListener(new SendButtonListener());
+        messageField.addActionListener(new SendButtonListener());
+
+
         // set up frame GUI
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600,500);
         frame.setVisible(true);
     }
+
+    // Send Button Action Listener
+    private class SendButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String message = messageField.getText();
+            out.println(message);
+            messageField.setText("");
+        }
+    }
+
+    // Incoming Message Handler
+    private class IncomingMessageHandler implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    // sends messages to the next line after submission
+                    String message = in.nextLine();
+                    chatArea.append(message + "\n");
+                }
+            } catch (Exception e) {
+                chatArea.append("Error receiving message: " + e.getMessage() + "\n");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.printf("hello world");
         String username = "jinx";
